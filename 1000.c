2568f389b46f2844bcd2ef605a9ebf9acf48229b
Charles Magalhães <charles.magalhaes.developer@gmail.com>
2024-11-09 11:51:44 -0300

    Hello World!

---
+++ 1000.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+//Bem-vindo ao beecrowd!
+
+//O seu primeiro programa em qualquer linguagem de programação normalmente é o
+//"Hello World!". Neste primeiro problema tudo o que você precisa fazer é imprimir esta mensagem na tela.
+
+int main()
+{
+    printf("Hello World!\n");
+
+    return 0;
+}
